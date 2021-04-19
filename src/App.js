237bc04e2066a9f69f8656ede887441f3a51f09c
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-19 13:06:27 -0400

    Trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/Login';
 
-class App extends React.Component {
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
+    }
+  }
+
   render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
+
     return (
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" component={Home}/>
-          </Switch>
-        </BrowserRouter>
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
+
+          </div>
+        </Router>
     );
   }
+
 }
 
 export default App;
