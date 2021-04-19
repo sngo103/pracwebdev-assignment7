cb71769f0849f4385c314057e3d54396d1400fcc
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-18 21:30:21 -0400

    Trying to Deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route path="/" component={Home}/>
           </Switch>
         </BrowserRouter>
     );
